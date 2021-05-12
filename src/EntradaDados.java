2e61dbbfb54f3996f749aab14a3b9b77f609c63c
Entra21 <entra21@proway.treina>
2021-05-12 19:45:26 -0300

    aprendendo quebra de linha

---
+++ src/EntradaDados.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class EntradaDados {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        int x;
+        String s1,s2,s3;
+
+        x = scanner.nextInt();
+        scanner.nextLine();
+        s1 = scanner.nextLine();
+        s2 = scanner.nextLine();
+        s3 = scanner.nextLine();
+
+        System.out.println("Dados Digitados");
+        System.out.println(x);
+        System.out.println("s1->" +s1);
+        System.out.println("s2->" +s2);
+        System.out.println("s3->" +s3);
+
+
+
+
+        scanner.close();
+
+
+    }
+}
