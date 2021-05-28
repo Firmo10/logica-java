cacd00183f9c5dadff952d3cd2d5953750ce883c
Entra21 <entra21@proway.treina>
2021-05-28 20:30:01 -0300

    saudacao

---
+++ src/Saudacao.java
@@ -0,0 +1,21 @@
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Saudacao {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a hora atual");
+        int horaAtual = scanner.nextInt();
+
+        if (horaAtual < 12 && horaAtual >= 6){
+            System.out.println("Bom dia");
+        }else if (horaAtual >= 12 && horaAtual < 18){
+            System.out.println("Boa tarde");
+        }else{
+            System.out.println("Boa noite");
+        }
+    }
+}
